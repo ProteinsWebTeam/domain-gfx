95277952a8b21266e80630997bdca6509d4d69df
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2019-06-07 09:37:15 +0100

    updating polyfills

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,4 +1,2 @@
-import 'core-js/modules/es6.symbol';
-import 'core-js/modules/web.dom.iterable';
-import 'core-js/modules/es6.set';
-import 'core-js/modules/es6.number.is-finite';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
